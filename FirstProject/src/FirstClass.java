0ce7f7b2ae032414a136d87acb5e95037b0efb9f
Rahul <test@gmail.com>
2020-06-19 04:46:31 -0400

    Working code for 1 to 10 random integers printing

--- FirstProject/src/FirstClass.java
+++ FirstProject/src/FirstClass.java
@@ -7,34 +7,32 @@ public class FirstClass {
 		Random dice = new Random();
 		float floatnumber;
 		int intnumber;
-		int maximum = 5;
-		int count=0;
+		int maximum = 10;
+		int count= 0;
 		int repeatCount=0;
+		int mainLoop = 0;
 		
 		int arrayint[] = new int[100];
 		
 		while(count<maximum)
 		{
 			
-
-		for(int mainLoop=0; mainLoop<count+1;mainLoop++) {
-		
 			//System.out.println("Main Loop " + mainLoop + " Starts");
 			
 			floatnumber = dice.nextFloat();
 			
 			intnumber = (int) (floatnumber *10);
 						
-			arrayint[mainLoop]= intnumber;
+			arrayint[count]= intnumber;
 			
-			repeatCount=0;
+					repeatCount=0;
 			
 			//System.out.println(intnumber + " number generated ");
 
 			
 			for(int arraycheck=0;arraycheck<count;arraycheck++) {
 				
-				//System.out.println("Random Integer Now is " + intnumber);
+				//System.out.println("New Random Integer Now is " + intnumber + " Check against " + arrayint[arraycheck]);
 				//System.out.println("Array Element " + arraycheck + " is " + arrayint[arraycheck]);
 				
 				if(arrayint[arraycheck]==intnumber && mainLoop>0)
@@ -44,20 +42,20 @@ public class FirstClass {
 				//System.out.println("RepeatCheck value is " + repeatCount);
 			
 				}
-			   //System.out.println("loop " + mainLoop + " over");
-			
+			   			
 						
-				if(repeatCount<1) {
-				System.out.println("Random number " + intnumber + " printed in loop " + mainLoop);
-				repeatCount = 0;
-				}
+				if(repeatCount==0) {
 				
-				if(repeatCount<1 && count<maximum)
-					count++;
+				System.out.println("Random number " + intnumber + " printed in loop " + mainLoop);
+				arrayint[count]= intnumber;
+				count++;
+				
+					}
+				
+				//System.out.println("loop " + mainLoop + " over");
 							
-		} //mainLoop Close
-		
-		}
+				mainLoop++;
+		} // while loop close
 
 	} // main close
 
